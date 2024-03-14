e4bdf4e5ae3e5f7b6f7dfafca18009428c687c7f
Scott <sbaroni2004@gmail.com>
2024-03-13 22:46:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,38 @@ img = Image.new("RGB", (600, 600))
 
 room_num = int(input("Enter number of rooms:"))
 
+
 for x in range(room_num):
     #random size within certain limits:
-    x0 = random.randint(10, 300)
-    x1 = x0 + random.randint(100, 290)
-    y0 = random.randint(10, 300)
-    y1 = y0 + random.randint(100, 290)
-        
+    if x == 0:
+        #Top Left Quadrant:
+        x0 = random.randint(10, 130)
+        x1 = random.randint(150, 290)
+        y0 = random.randint(10, 130)
+        y1 = random.randint(150, 290)
+
+    if x == 1:
+        #Bottom Left Quadrant
+        x0 = random.randint(10, 130)
+        x1 = random.randint(150, 290)
+        y0 = random.randint(310, 430)
+        y1 = random.randint(450, 590)
+
+    if x == 2:
+        #Top Right Quadrant
+        x0 = random.randint(310, 430)
+        x1 = random.randint(450, 590)
+        y0 = random.randint(10, 130)
+        y1 = random.randint(150, 290)
+
+    if x == 3:
+        #Bottom Right Quadrant
+        x0 = random.randint(310, 430)
+        x1 = random.randint(450, 590)
+        y0 = random.randint(310, 430)
+        y1 = random.randint(450, 590)
+
+
     #This is the size of the rectangle:
     shape = [(x0, y0), (x1, y1)] 
 
